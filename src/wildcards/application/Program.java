db93a3539d445f13ddaa1220432ae51bbf09c5e0
RodolfoLubeck <rcflubeck@gmail.com>
2024-11-22 01:38:17 -0300

    feature: strlist same print method

--- src/wildcards/application/Program.java
+++ src/wildcards/application/Program.java
@@ -7,7 +7,12 @@ public class Program {
     public static void main(String[] args) {
         List<Integer> myInts = Arrays.asList(5,2,10);
         printList(myInts);
+
+        List<String> myStrs = Arrays.asList("Maria", "Alex", "Bob");
+        printList(myStrs);
     }
+
+
 
     public static void printList(List<?> list){
         for (Object obj : list){
